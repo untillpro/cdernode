a8fa4b4d84b8d5d7526aa024f565858bd39cb6c1
kvv <v.kuslin@untill.com>
2019-08-23 11:50:19 +0300

    build.sh changed

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ cd ./cder
 go build
 echo "Create docker image"
 cd ..
-version=$(git describe --abbrev=0)
-docker build -t "${DOCKER_LOGIN}"/cdernode:"$version" .
+ver=$(cat version)
+if [[ $ver == *"SNAPSHOT"* ]]; then
+  echo "Version can't contain SNAPSHOT: $ver"
+  exit 1
+fi
+docker build -t "${DOCKER_LOGIN}"/cdernode:"$ver" .
 echo "Push image"
-docker push "${DOCKER_LOGIN}"/cdernode:"$version"
-echo 'Change version in compose file'
-sed -i "s/\(cdernode:\)\(.*\)/\1$version/" docker-compose.yml
+docker push "${DOCKER_LOGIN}"/cdernode:"$ver"
+echo 'Change ver in compose file'
+sed -i "s/\(cdernode:\)\(.*\)/\1$ver/" docker-compose.yml
